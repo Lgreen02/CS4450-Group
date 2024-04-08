9030365e9e435a454e4451e5599d2ebc85e7e6fd
Gerardo Solis <56048406+Sol-Gerardo@users.noreply.github.com>
2024-04-07 18:12:22 -0700

    Updated FPCameraController

--- JavaApplication4/src/javaapplication4/FPCameraController.java
+++ JavaApplication4/src/javaapplication4/FPCameraController.java
@@ -97,7 +97,7 @@ public class FPCameraController {
         float dt = 0.0f; //length of frame
         float lastTime = 0.0f; // when the last frame was
         long time = 0;
-        float mouseSensitivity = 0.09f;
+        float mouseSensitivity = 0.01f;
         float movementSpeed = .35f;
         //hide the mouse
         Mouse.setGrabbed(true);
@@ -152,12 +152,29 @@ public class FPCameraController {
     }
     private void render() {
         try{
+            float x = 2f;
+            
             glBegin(GL_QUADS);
-            glColor3f(1.0f,0.0f,1.0f);
-            glVertex3f( 1.0f,-1.0f,-1.0f);
-            glVertex3f(-1.0f,-1.0f,-1.0f);
-            glVertex3f(-1.0f, 1.0f,-1.0f);
-            glVertex3f( 1.0f, 1.0f,-1.0f);
+                // TOP SIDE OF SQUARE
+                glColor3f(1.0f,0.0f,1.0f);
+                glVertex3f(x, x, -x);
+                glVertex3f(-x, x,-x);
+                glVertex3f( -x, x, x);
+                glVertex3f(x, x, x);
+                
+                // BACK OF SQUARE 
+                glColor3f(1.0f,1.0f,1.0f);
+                glVertex3f(x, x, -x); // top right
+                glVertex3f(-x, x,-x); // top left 
+                glVertex3f(-x,-x, -x); // bottom left
+                glVertex3f(x, -x, -x); // bottom right 
+                
+                // LEFT OF SQUARE 
+                
+                // RIGHT OF SQUARE 
+                
+                // FRONT OF SQUARE 
+                
             glEnd();
         }catch(Exception e){
             }
