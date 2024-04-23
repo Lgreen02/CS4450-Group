2620173df0b607cf323594e46630b262d0cb87c0
CabooseShep <nhortua@cpp.edu>
2024-04-23 00:34:25 -0700

    added an additional block type, meant to push with last commit

--- JavaApplication4/src/javaapplication4/Block.java
+++ JavaApplication4/src/javaapplication4/Block.java
@@ -19,11 +19,12 @@ public class Block {
     
     public enum BlockType{
         BlockType_Grass(0),
-        BlockType_Sand(1),
-        BlockType_Water(2),
-        BlockType_Dirt(3),
+        BlockType_Dirt(1),
+        BlockType_Sand(2),
+        BlockType_Water(3),
         BlockType_Stone(4),
-        BlockType_Bedrock(5);
+        BlockType_Iron(5),
+        BlockType_Bedrock(6);
         
         private int BlockID;
         
