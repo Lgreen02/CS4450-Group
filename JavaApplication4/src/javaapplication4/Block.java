bf14326259f7e71ae994e8a67cf412f98208415d
CabooseShep <nhortua@cpp.edu>
2024-04-22 23:09:31 -0700

    added more basic functionality to Block class

--- JavaApplication4/src/javaapplication4/Block.java
+++ JavaApplication4/src/javaapplication4/Block.java
@@ -1,21 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/***************************************************************
+* file: Block.java
+* author: Carson Green, Gerardo Solis, Nick Hortua
+* class: CS 4450 – Computer Graphics
+*
+* assignment: Semester Project
+* date last modified: 4/22/2024
+*
+* purpose: This class is responsible for defining functions regarding for blocks
+* to be generated in our program, such as type, ID, and coordinates.
+****************************************************************/
+
 package javaapplication4;
 
-/**
- *
- * @author Admin
- */
 public class Block {
     private boolean status;
-    private String type;
+    private BlockType type;
+    private float x,y,z;
     
-    public Block(String type){ this.type = type;}
+    public enum BlockType{
+        BlockType_Grass(0),
+        BlockType_Sand(1),
+        BlockType_Water(2),
+        BlockType_Dirt(3),
+        BlockType_Stone(4),
+        BlockType_Bedrock(5);
+        
+        private int BlockID;
+        
+        BlockType(int i) {
+            BlockID = i;
+        }
+        
+        public int GetID(){
+            return BlockID;
+        }
+        
+        public void SetID(int i){
+            BlockID = i;
+        }
+    }
     
-    public String getType(){
-        return type;
+    public Block(BlockType type){ 
+        this.type = type;
+    }
+    
+    public void setCoords(float x, float y, float z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    public boolean Status(){
+        return status;
+    }
+    
+    public void SetStatusActive(boolean active){
+        status = active;
+    }
+    
+    public int GetID(){
+        return type.GetID();
     }
     
 }
